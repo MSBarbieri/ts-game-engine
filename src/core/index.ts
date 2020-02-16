c68cfd5d892b8a7eeadf387b26f75c41d18e68ae
MSBarbieri <matheus6152@hotmail.com>
2020-02-16 16:08:40 -0300

    base classes

---
+++ src/core/index.ts
@@ -0,0 +1,4 @@
+export * from './component';
+export * from './scene';
+export * from './prefab';
+export * from './engine';
---
+++ src/core/scene.ts
@@ -0,0 +1,7 @@
+export function Scene() {
+    
+}
+
+export class SceneManager {
+    
+}
